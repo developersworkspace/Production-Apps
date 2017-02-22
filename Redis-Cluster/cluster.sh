c0bfbe35c0083fcbf38efc84f20c32f9456a02ba
Barend Erasmus <developersworkspace@gmail.com>
2017-02-22 10:46:04 +0200

    Update

--- Redis-Cluster/cluster.sh
+++ Redis-Cluster/cluster.sh
@@ -1,4 +1,6 @@
 cd /tmp/redis-stable/src
+mv redis-trib.rb redis-trib.temp
+sed "s/yes_or_die \"Can I set the above configuration?\"/# yes_or_die \"Can I set the above configuration?\"/g" redis-trib.temp > redis-trib.rb
 
 IFS='|' read -a ipAddresses <<< "$2"
 IFS='|' read -a ports <<< "$3"
