bc4d254e6a8653acad8bf1016acf74177abe7349
Barend Erasmus <developersworkspace@gmail.com>
2017-02-22 10:50:08 +0200

    Updates cluster script

--- Redis-Cluster/cluster.sh
+++ Redis-Cluster/cluster.sh
@@ -1,10 +1,19 @@
+# Change to redis source directory
 cd /tmp/redis-stable/src
+
+# Rename 'redis-trib.rb' to 'redis-trib.temp'
 mv redis-trib.rb redis-trib.temp
+
+# Comment 'Can I set the above configuration?' line
 sed "s/yes_or_die \"Can I set the above configuration?\"/# yes_or_die \"Can I set the above configuration?\"/g" redis-trib.temp > redis-trib.rb
 
+# Split IP Addresses
 IFS='|' read -a ipAddresses <<< "$2"
+
+# Split Ports
 IFS='|' read -a ports <<< "$3"
 
+# Assign varible 'replicas' to first argument
 replicas=$1
 
 for ipAddress in "${ipAddresses[@]}"
@@ -15,4 +24,5 @@ do
     done
 done
 
+# Execute 'redis-trib.rb'
 ./redis-trib.rb create --replicas $replicas$ipAddressesAndPorts
